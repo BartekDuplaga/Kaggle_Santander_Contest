84886f69fd12b65fdfc30213b4217f526cad1687
BartłomiejDuplaga <54235802+BartekDuplaga@users.noreply.github.com>
2019-09-02 11:52:50 +0200

    Update Santander_Script.py

--- Santander_Script.py
+++ Santander_Script.py
@@ -5,7 +5,7 @@ Created on Mon Mar 25 10:59:26 2019
 
 @author: bartek
 """
-import numpy as np # linear algebra
+import numpy as np 
 import pandas as pd
 import lightgbm as lgb
 import matplotlib.pyplot as plt
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 train_df = pd.read_csv('/Users/bartek/Desktop/Kaggle/Santander/Santander/train.csv')
 test_df = pd.read_csv('/Users/bartek/Desktop/Kaggle/Santander/Santander/test.csv')
 
-# data augementation
+# Data augementation
 def augment(x,y,t=2):
     xs,xn = [],[]
     for i in range(t):
@@ -51,15 +51,14 @@ y_train = target
 X_tr, y_tr = augment(X_train.values, y_train.values)
 print(X_tr.shape, y_tr.shape, y_tr.mean())
 
-#podział na zbiory
+#Train & Test Split
 X_train, X_test, y_train, y_test = train_test_split(X_tr, y_tr, test_size=0.2, random_state=42)
 print(X_train.shape) 
 print(X_test.shape) 
 print(y_train.mean())
 print(y_test.mean())
 
-#badanie LGBM
-
+#LGBM params
 parameters = {
     'objective': 'binary',
     'metric': 'auc',
@@ -72,41 +71,6 @@ parameters = {
     'feature_fraction':0.5,
     'lambda_l2': 1  
 }
-lgb_train = lgb.Dataset(X_train, y_train)
-lgb_model = lgb.train(parameters,lgb_train,num_boost_round=1000,verbose_eval=5)
-
-y_hat = lgb_model.predict(X_test, num_iteration = lgb_model.best_iteration)
-y_hat_df = pd.DataFrame(y_hat)
-y_hat_df.hist(normed=True, cumulative= True, bins=100)
-y_hat_df_1 = (y_hat_df>0.5).astype('int')
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-cm = confusion_matrix(y_test, y_hat_df_1)
-print(cm)
-a_score=accuracy_score(y_test,y_hat_df_1)
-print(a_score)
-
-#Krzywa ROC
-import sklearn.metrics as metrics
-# calculate the fpr and tpr for all thresholds of the classification
-probs = lgb_model.predict(X_test, num_iteration = lgb_model.best_iteration)
-fpr, tpr, threshold = metrics.roc_curve(y_test, probs)
-roc_auc = metrics.auc(fpr, tpr)
-# method I: plt
-plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.show()
-
-sub = pd.DataFrame({"ID_code": test_df.ID_code.values})
-sub["target"] = lgb_model.predict(test_df.drop('ID_code',axis=1))
-sub.to_csv('Kaggle_pred1.csv', index=False)
 
 ### LIGHT GBM
 
@@ -128,8 +92,47 @@ y_hat /= len(features)
 sub = pd.DataFrame({"ID_code": test_df.ID_code.values})
 sub["target"] = y_hat
 sub.to_csv('Kaggle_pred1.csv', index=False)
+lgb_train = lgb.Dataset(X_train, y_train)
+lgb_model = lgb.train(parameters,lgb_train,num_boost_round=1000,verbose_eval=5)
+
+y_hat = lgb_model.predict(X_test, num_iteration = lgb_model.best_iteration)
+y_hat_df = pd.DataFrame(y_hat)
+y_hat_df.hist(normed=True, cumulative= True, bins=100)
+y_hat_df_1 = (y_hat_df>0.5).astype('int')
+
+#Confusion Matrix, Accuracy
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+cm = confusion_matrix(y_test, y_hat_df_1)
+print(cm)
+a_score=accuracy_score(y_test,y_hat_df_1)
+print(a_score)
+
+#ROC Curve 
+import sklearn.metrics as metrics
+# calculate the fpr and tpr for all thresholds of the classification
+probs = lgb_model.predict(X_test, num_iteration = lgb_model.best_iteration)
+fpr, tpr, threshold = metrics.roc_curve(y_test, probs)
+roc_auc = metrics.auc(fpr, tpr)
+# method I: plt
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
+#Exporting predictions
+sub = pd.DataFrame({"ID_code": test_df.ID_code.values})
+sub["target"] = lgb_model.predict(test_df.drop('ID_code',axis=1))
+sub.to_csv('Kaggle_pred1.csv', index=False)
+
+
 """
-# Tunningowanie hiperparametrów
+# Hyperparameter tuning 
 from sklearn.model_selection import GridSearchCV
 param_grid = [
         {'min_data_in_leaf': [15,50,150],
@@ -148,9 +151,4 @@ cvres = grid_search.cv_results_
 for mean_score, params in zip(cvres['mean_test_score'],cvres['params']):
     print(np.sqrt(-mean_score), params)
 
-        
 
-
-
-
-### LightGBM z Scikit Learn
